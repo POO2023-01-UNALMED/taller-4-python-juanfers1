37fd80fe70f369be3143605eacfa427c006b312a
Juan Fernando Castaño Durán <54716226+juanfers1@users.noreply.github.com>
2023-03-05 22:03:45 -0500

    :+1:

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,10 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    # Este metodo es para imprimir el nombre y el salon de la asignatura 
+    def __str__(self):
+        string= self._nombre + " " + self._salon
+        return string
